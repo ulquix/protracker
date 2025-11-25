40a438a8bcac91fc39a3af01e9afc0463dee47ea
Prince Yadav <princeyadav392005@gmail.com>
2025-11-25 23:36:02 +0530

    vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App.tsx'
 import { ThemeProvider } from 'next-themes'
-
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme='system' attribute='class' enableSystem={true} storageKey='food-theme'>
-
+<Analytics/>
+<SpeedInsights />
     <App />
     </ThemeProvider>
   </StrictMode>,
